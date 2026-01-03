3eede5de1b985255ce42139af40348f82a127b83
lewismcdonaldUU <McDonald-L23@ulster.ac.uk>
2026-01-03 12:31:54 +0000

    Added Student and Admin classes.

---
+++ Quiz_App/Admin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_App
+{
+    internal class Admin
+    {
+        private DateTime loginDate;
+
+        public Admin(string username, string password, string email, string role, DateTime loginDate)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            Role = role;
+            this.loginDate = loginDate;
+        }
+
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Email { get; private set; }
+        public string Role { get; private set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("=== Admin ===");
+            Console.WriteLine($"Username: {Username}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Role: {Role}");
+            Console.WriteLine($"Login Date: {loginDate}");
+        }
+
+        public void UpdateUser(string username, string password, string email, string role, DateTime loginDate)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            Role = role;
+            this.loginDate = loginDate;
+        }
+    }
+
+}
---
+++ Quiz_App/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_App
+{
+    internal class Student
+    {
+        private string status;
+
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Email { get; private set; }
+        public string Role { get; private set; }
+
+        public Student(string username, string password, string email, string role, string status)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            Role = role;
+            this.status = status;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("=== Student ===");
+            Console.WriteLine($"Username: {Username}");
+            Console.WriteLine($"Email: {Email}");
+            Console.WriteLine($"Role: {Role}");
+            Console.WriteLine($"Status: {status}");
+        }
+
+        public void UpdateUser(string username, string password, string email, string role, string status)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            Role = role;
+            this.status = status;
+        }
+    }
+
+}
