c28f4be41b28ad25713e1fcc3493167a914b01ca
EoinDPCarey <carey-e12@ulster.ac.uk>
2026-01-04 11:44:15 +0000

    Quiz should work now

--- Quiz_App/Category.cs
+++ Quiz_App/Category.cs
@@ -8,15 +8,15 @@ namespace Quiz_App
 {
     public class Category
     {
-        // Fields / Properties
-        public int CategoryID { get; }          // read-only
+        
+        public int CategoryID { get; }          
         public string Name { get; private set; }
         public string Description { get; private set; }
 
-        // Static counter to auto-generate IDs
+        
         private static int _idCounter = 1;
 
-        // Constructor
+       
         public Category(string name, string description)
         {
             CategoryID = _idCounter++;
@@ -24,7 +24,6 @@ namespace Quiz_App
             Description = description;
         }
 
-        // DisplayCategory()
         public void DisplayCategory()
         {
             Console.WriteLine("=== Category Information ===");
@@ -33,7 +32,6 @@ namespace Quiz_App
             Console.WriteLine($"Description: {Description}");
         }
 
-        // UpdateCategory(name: string, description: string)
         public void UpdateCategory(string name, string description)
         {
             Name = name;
